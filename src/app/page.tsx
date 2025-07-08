a83c88d9af088f4dce4113ef45df06bbc18832ca
Doan Tan Ty <tydt@mynavitechtus.com>
2025-07-08 15:43:54 +0700

    Refactor DataComponent to include API data fetching and integrate SearchComponent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import { Suspense } from "react";
 import DataComponent from "./components/DataComponent";
-import SearchComponent from "./components/SearchComponent";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 type Props = {
@@ -8,78 +8,12 @@ type Props = {
 
 export default async function Home(props: Props) {
   const searchParams = await props.searchParams;
-  const test = searchParams.test || "";
-  const random = Math.random().toString(36).substring(2, 15);
-  const END_POINTS = [
-    "https://dummy-json.mock.beeceptor.com/posts",
-    "https://dummy-json.mock.beeceptor.com/comments",
-    "https://dummy-json.mock.beeceptor.com/companies",
-    "https://dummy-json.mock.beeceptor.com/users",
-    "https://dummy-json.mock.beeceptor.com/todos",
-    "https://dummy-json.mock.beeceptor.com/quotes",
-    "https://jsonplaceholder.typicode.com/todos?abc=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/users?abc=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/posts?abc=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/comments?abc=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/albums?abc=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/photos?abc=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/todos?def=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/users?def=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/posts?def=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/comments?def=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/albums?def=" +
-      random +
-      "&test=" +
-      test,
-    "https://jsonplaceholder.typicode.com/photos?def=" +
-      random +
-      "&test=" +
-      test,
-  ];
-
-  const promises = END_POINTS.map((endpoint) =>
-    fetch(endpoint, {
-      headers: { "Content-Type": "application/json" },
-      cache: "no-store",
-    }).then((res) => res.json())
-  );
-
-  const data = await Promise.all(promises);
 
   return (
     <div>
-      <SearchComponent />
-      <DataComponent data={data} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <DataComponent searchParams={searchParams} />
+      </Suspense>
     </div>
   );
 }

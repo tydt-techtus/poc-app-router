72a97dc0af0d59f9fc4f4240d91f9d81698decde
Doan Tan Ty <tydt@mynavitechtus.com>
2025-07-08 13:32:09 +0700

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+import DataComponent from "./components/DataComponent";
+
+export const experimental_ppr = true;
+
+export default async function Home() {
+  const random = Math.random().toString(36).substring(2, 15);
+  const END_POINTS = [
+    "https://jsonplaceholder.typicode.com/todos?abc=" + random,
+    "https://jsonplaceholder.typicode.com/users?abc=" + random,
+    "https://jsonplaceholder.typicode.com/posts?abc=" + random,
+    "https://jsonplaceholder.typicode.com/comments?abc=" + random,
+    "https://jsonplaceholder.typicode.com/albums?abc=" + random,
+    "https://jsonplaceholder.typicode.com/photos?abc=" + random,
+    "https://jsonplaceholder.typicode.com/todos?def=" + random,
+    "https://jsonplaceholder.typicode.com/users?def=" + random,
+    "https://jsonplaceholder.typicode.com/posts?def=" + random,
+    "https://jsonplaceholder.typicode.com/comments?def=" + random,
+    "https://jsonplaceholder.typicode.com/albums?def=" + random,
+    "https://jsonplaceholder.typicode.com/photos?def=" + random,
+  ];
+
+  const promises = END_POINTS.map((endpoint) =>
+    fetch(endpoint, {
+      headers: { "Content-Type": "application/json" },
+      cache: "no-store",
+    }).then((res) => res.json())
+  );
+
+  const data = await Promise.all(promises);
+
+  return (
+    <div>
+      <DataComponent data={data} />
+    </div>
+  );
+}
